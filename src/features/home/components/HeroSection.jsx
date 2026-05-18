0190c5a9d3249d782ef891fb2e65a83d457c57a7
B-MO56 <153782269+bkmorris2456@users.noreply.github.com>
2026-05-17 20:49:04 -0400

    Moved About section into its separate component, HERO SECTION

---
+++ src/features/home/components/HeroSection.jsx
@@ -0,0 +1,83 @@
+import { Box, Grid, Typography } from "@mui/material";
+import { motion } from "framer-motion";
+import UpdatedPhoto from "../../../images/linkedin-headshot.webp";
+
+function HeroSection() {
+  return (
+    <Box
+      id="home"
+      component="main"
+      sx={{
+        flex: 1,
+        p: { xs: 2, sm: 4, md: 6 },
+        maxWidth: "1200px",
+        mx: "auto",
+        marginBottom: "10vh",
+      }}
+    >
+      <Grid container spacing={4} alignItems="center">
+        <Grid item xs={12} sm={12} sx={{ display: "flex", justifyContent: "center" }}>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <Typography
+              variant="h1"
+              sx={{
+                textAlign: "center",
+                marginTop: "10vh",
+                marginBottom: "10vh",
+                fontSize: { xs: "35px", sm: "45px" },
+              }}
+            >
+              My name is Blake Morris, and I'm a Software Developer.
+            </Typography>
+          </motion.div>
+        </Grid>
+
+        <Grid item xs={12} sm={6} sx={{ display: "flex", justifyContent: "center" }}>
+          <Box
+            component="img"
+            src={UpdatedPhoto}
+            alt="My Picture"
+            sx={{
+              borderRadius: "50%",
+              width: { xs: "200px", sm: "350px" },
+              height: { xs: "200px", sm: "350px" },
+              objectFit: "cover",
+            }}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <Typography variant="body1" sx={{ textAlign: "justify", fontSize: { xs: "14px", sm: "16px" }, marginBottom: "2vh" }}>
+              I have a deep passion for designing and developing effective applications that not only assist with problems and make every-day life easier,
+              but also help me tackle tough problems that help me learn and improve as a developer. I take great pride in knowing that I continuously improve
+              in the work that I do, and I always look for the greatest chances to help me grow.
+            </Typography>
+
+            <Typography variant="body1" sx={{ textAlign: "justify", fontSize: { xs: "14px", sm: "16px" }, marginBottom: "2vh" }}>
+              Currently, I work as a contracting software developer, actively seeking out clients and individuals who can benefit from tailored software solutions
+              to enhance their business or daily lives. I use these opportunities not only to solve real-world problems but also to explore new technologies and build
+              innovative products.
+            </Typography>
+
+            <Typography variant="body1" sx={{ textAlign: "justify", fontSize: { xs: "14px", sm: "16px" } }}>
+              In the past couple of years, I've had the pleasure of accruing knowledge and experience during my time studying at Michigan State University,
+              working with Ally Financial, and continuing my work with other clients. If I'm not developing projects or studying Data Structures and Algorithms,
+              then I'm most likely deep diving into one of my hobbies!
+            </Typography>
+          </motion.div>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default HeroSection;
